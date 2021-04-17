327f1055578f81f2168121ef3958a42f255684fc
jennifer_b <jennifer.brunker@gmail.com>
2021-04-17 18:56:06 -0400

    Added js to retrieve repo data using github API and display on page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //div where profile info appears
 const overview = document.querySelector(".overview");
 const username = "jbrunker-code";
+const repoList = document.querySelector(".repo-list");
 
 async function getData (){
     const retrieve = await fetch (`https://api.github.com/users/${username}`);
@@ -25,4 +26,22 @@ function displayData (userData){
       <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
     </div>`
     overview.append(div);
+};
+
+async function getRepos (){
+    const retrievedRepoData = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await retrievedRepoData.json();
+    console.log(repos);
+    displayRepos(repos);
+};
+
+getRepos();
+
+function displayRepos (repos){
+    for (let repo of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`
+        repoList.append(li);
+    }
 }
